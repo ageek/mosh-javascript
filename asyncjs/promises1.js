b8448286be090f70fea0a40c358460edb7fd6dd2
Ahmed <re.90@live.com>
2025-01-10 08:45:50 +0530

    show response.status

--- asyncjs/promises1.js
+++ asyncjs/promises1.js
@@ -1,10 +1,15 @@
 // fetch API retursn a Promise
-const id = Math.ceil(Math.random() * 100) % 10;
+const id = Math.ceil(Math.random() * 100) % 100;
 console.log(id);
+// for error:
+// fetch(`https://jsonplaceholder.typicode.com/todos/${id}0`)
+
+// for random value
 fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
   // response.json() returns a Promise
   .then((response) => {
-    if (!response.ok) throw new Error("Network response was not ok");
+    if (!response.ok)
+      throw new Error(`Network response was not ok: ${response.status}`);
     else return response.json();
   })
   .then((data) => {
