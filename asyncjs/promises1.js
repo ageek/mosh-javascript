72751af3fe85abc6da7f708be53813a81c0cde0e
Ahmed <re.90@live.com>
2025-01-10 08:36:03 +0530

    randomized data fetching - promise

--- asyncjs/promises1.js
+++ asyncjs/promises1.js
@@ -1,5 +1,12 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
+// fetch API retursn a Promise
+const id = Math.ceil(Math.random() * 100) % 10;
+console.log(id);
+fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  // response.json() returns a Promise
+  .then((response) => {
+    if (!response.ok) throw new Error("Network response was not ok");
+    else return response.json();
+  })
   .then((data) => {
     console.log("Data successfully fetched:", data);
   })
